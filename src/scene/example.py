3840d170a3ba5c8e681948e7a92a5290be834824
congee524 <congee524@gmail.com>
2022-04-02 03:47:40 +0800

    add texture

--- src/scene/example.py
+++ src/scene/example.py
@@ -1,11 +1,11 @@
 from vec import Point, Color
 from sphere import Sphere
 from material import Lambertian, Metal
-from texture import ConstantTexture
+from texture import ConstantTexture, CheckerTexture
 from hittable import HittableList
 
 mat_1 = Lambertian(ConstantTexture(Color(0.1, 0.2, 0.5)))
-mat_2 = Metal(ConstantTexture(Color(0.8, 0.8, 0)), 0.3)
+mat_2 = Metal(CheckerTexture(ConstantTexture(Color(0.2, 0.3, 0.1)), ConstantTexture(Color(0.9, 0.9, 0.9))), 0.3)
 mat_3 = Lambertian(ConstantTexture(Color(0., 0., 1.)))
 mat_4 = Lambertian(ConstantTexture(Color(1, 0, 0)))
 
