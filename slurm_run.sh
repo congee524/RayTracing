9f4327436dd46c4a7a5d04293123c0617bae8510
congee524 <congee524@gmail.com>
2022-06-22 14:11:49 +0800

    fix bug

--- slurm_run.sh
+++ slurm_run.sh
@@ -2,4 +2,4 @@ OUTPUT=$1
 SAMPLES=$2
 
 srun -p video --job-name=rt --ntasks=1 --cpus-per-task=32 --async \
-    python src\main.py --output ${OUTPUT} --samples ${SAMPLES}
+    python src/main.py --output ${OUTPUT} --samples ${SAMPLES}
